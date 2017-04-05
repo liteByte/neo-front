45682b50e3a9bc63a005b24ce6f43f35c92801c8
Gonza <gonzalo.medinarosa@litebyte.us>
2017-04-05 16:05:57 -0300

    Implement neo list with sample image

--- src/components/ListSection/ListSection.js
+++ src/components/ListSection/ListSection.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import InfoButton from "../InfoButton";
+import {GridList, GridTile} from 'material-ui/GridList';
 
 const sectionStyle = {
   width: '50%',
@@ -13,11 +14,59 @@ const titleStyle = {
   fontWeight: 'normal',
 };
 
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: "100%",
+    height: "100%",
+    overflowY: 'auto',
+  },
+};
+
 export default class ListSection extends Component {
+
+  neos = [
+    {
+      img: 'assets/header_logo.svg'
+    },
+    {
+      img: 'assets/header_logo.svg'
+    },
+    {
+      img: 'assets/header_logo.svg'
+    },
+    {
+      img: 'assets/header_logo.svg'
+    },
+    {
+      img: 'assets/header_logo.svg'
+    },
+    {
+      img: 'assets/header_logo.svg'
+    },
+    {
+      img: 'assets/header_logo.svg'
+    },
+  ];
+
   render() {
     return (
       <section style={sectionStyle}>
         <h2 style={titleStyle}>Near Earth Objects<InfoButton/></h2>
+        <div style={styles.root}>
+          <GridList cellHeight={50} cols={0} style={styles.gridList}
+          >
+            {this.neos.map((neo, index) => (
+              <GridTile key={index}>
+                <img src={neo.img} alt="Neo"/>
+              </GridTile>
+            ))}
+          </GridList>
+        </div>
       </section>
     );
   }
