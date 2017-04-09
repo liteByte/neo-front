902a22bc66be3076a7020296573c9cdd9652717e
Gonza <gonzalo.medinarosa@litebyte.us>
2017-04-08 23:10:36 -0300

    Make AboutUs dialog scrollable

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -16,9 +16,15 @@ const titleStyle = {
   textAlign: 'center',
 };
 
+const bodyStyle = {
+  ...style,
+  position: 'relative',
+};
+
 const dividerStyle = {
+  width: '80%',
   height: 2,
-  margin: '0 0 15px 0',
+  position: 'fixed',
 };
 
 const containerStyle = {
@@ -26,6 +32,11 @@ const containerStyle = {
   maxWidth: 512,
   textAlign: 'center',
   color: 'rgba(255, 255, 255, .9)',
+};
+
+const earthStyle = {
+  marginTop: 15,
+  transform: 'translateX(6px)',
 };
 
 const spanStyle = {
@@ -44,14 +55,15 @@ export default class AboutUs extends Component {
         title="ABOUT US"
         open={this.props.open}
         onRequestClose={() => this.props.setOpen(false)}
+        autoScrollBodyContent={true}
         contentStyle={style}
-        bodyStyle={style}
+        bodyStyle={bodyStyle}
         titleStyle={titleStyle}
         actionsContainerStyle={style}
       >
         <Divider style={dividerStyle}/>
         <div style={containerStyle}>
-          <img src="/assets/tierra_sinnubes.svg" alt="Where we are"/>
+          <img src="/assets/tierra_sinnubes.svg" alt="Where we are" style={earthStyle} height={150}/>
           <p>We are a small development company located in Buenos Aires, Argentina,
             focused on delivering quality software.</p>
           <p>
