5a46b22f12c8f16f8326dbc5d8f68e0069d297d9
Gonza <gonzalo.medinarosa@litebyte.us>
2017-04-17 11:41:40 -0300

    Show neo name

--- src/components/DetailSection/Name.js
+++ src/components/DetailSection/Name.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
+import Neo from '../ListSection/Neo';
+
+const height = 100;
+const neoWidth = 100;
+
+const containerStyle = {
+  height,
+  padding: 10,
+};
+
+const neoContainerStyle = {
+  width: neoWidth,
+  height: '100%',
+  float: 'left',
+};
+
+const nameContainerStyle = {
+  width: `calc(100% - ${neoWidth}px)`,
+  height: '100%',
+  float: 'left',
+};
+
+const idStyle = {
+  marginTop: 10,
+};
+
+const nameStyle = {
+  margin: '10px 0 0 0',
+  fontFamily: 'Dosis, sans-serif',
+  fontSize: '2em',
+};
 
 export default class Name extends Component {
   render() {
     return (
-      <div/>
+      <div style={containerStyle}>
+        <div style={neoContainerStyle}>
+          <Neo data={this.props.data} cellSize={neoWidth}/>
+        </div>
+        <div style={nameContainerStyle}>
+          <div style={idStyle}>ID: {this.props.data.id}</div>
+          <h2 style={nameStyle}>{this.props.data.name}</h2>
+        </div>
+      </div>
     );
   }
 }
+
+Name.propTypes = {
+  data: React.PropTypes.object.isRequired,
+};
