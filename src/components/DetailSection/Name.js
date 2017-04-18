3a7dc4dcffbe2d0231ffc907c5e4c6a3e49bd2bc
Gonza <gonzalo.medinarosa@litebyte.us>
2017-04-18 16:08:17 -0300

    Move name to scss

--- src/components/DetailSection/Name.js
+++ src/components/DetailSection/Name.js
@@ -7,40 +7,14 @@ const neoWidth = 100;
 
 const containerStyle = {
   height: height + 20,
-  width: '100%',
-  padding: 10,
-  boxSizing: 'border-box',
-  float: 'left',
 };
 
 const neoContainerStyle = {
   width: neoWidth,
-  height: '100%',
-  float: 'left'
 };
 
 const nameContainerStyle = {
   width: `calc(100% - ${neoWidth}px)`,
-  height: '100%',
-  paddingLeft: 10,
-  boxSizing: 'border-box',
-  float: 'left',
-};
-
-const idStyle = {
-  marginTop: 10,
-  fontSize: '1.2em',
-};
-
-const nameStyle = {
-  margin: '8px 0 0 0',
-  fontFamily: 'Dosis, sans-serif',
-  fontSize: '2.5em',
-  width: '100%',
-  overflowX: 'hidden',
-  height: 50,
-  textOverflow: 'ellipsis',
-  whiteSpace: 'nowrap',
 };
 
 const dividerStyle = {
@@ -53,13 +27,13 @@ const dividerStyle = {
 export default class Name extends Component {
   render() {
     return (
-      <div style={containerStyle}>
-        <div style={neoContainerStyle}>
+      <div style={containerStyle} className="name-container">
+        <div style={neoContainerStyle} className="neo-container">
           <Neo data={this.props.data} cellSize={neoWidth}/>
         </div>
-        <div style={nameContainerStyle}>
-          <div style={idStyle}>ID: {this.props.data.id}</div>
-          <h2 style={nameStyle} title={this.props.data.name}>{this.props.data.name}</h2>
+        <div style={nameContainerStyle} className="name-container">
+          <div className="id">ID: {this.props.data.id}</div>
+          <h2 className="name" title={this.props.data.name}>{this.props.data.name}</h2>
         </div>
         <Divider style={dividerStyle}/>
       </div>
