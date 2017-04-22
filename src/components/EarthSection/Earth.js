58a7d441f60fe43b5317d06404edcc30293b9ff4
Gonza <gonzalo.medinarosa@litebyte.us>
2017-04-21 21:03:19 -0300

    Make Earth clickable like NEOs

---
+++ src/components/EarthSection/Earth.js
@@ -0,0 +1,57 @@
+import React, {Component} from "react";
+import Measure from "react-measure";
+import IconButton from "material-ui/IconButton";
+
+const buttonStyle = {
+  width: '100%',
+  height: '100%',
+  padding: 1,
+};
+
+const iconStyle = {
+  width: '100%',
+  height: '100%',
+  position: 'relative',
+};
+
+export default class Neo extends Component {
+
+  constructor(props) {
+    super(props);
+    this.updateState(props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.updateState(nextProps);
+  }
+
+  updateState(props) {
+    this.state = props.data;
+  }
+
+  getIconStyle(dimensions) {
+    const dimension = dimensions.width < dimensions.height ? dimensions.width : dimensions.height;
+    return {
+      ...iconStyle,
+      left: dimension * .04,
+    }
+  }
+
+  render() {
+    return (
+      <Measure>
+        { dimensions =>
+          <IconButton
+            style={buttonStyle}
+            iconStyle={this.getIconStyle(dimensions)}
+            onClick={() => {}}
+          >
+            <img src="/assets/tierra_nubes.svg" alt="Earth"/>
+          </IconButton>
+        }
+      </Measure>
+    );
+  }
+}
+
+Neo.propTypes = {};
--- src/components/EarthSection/Space.js
+++ src/components/EarthSection/Space.js
@@ -1,10 +1,19 @@
 import React, {Component} from "react";
 import Measure from "react-measure";
 import SpaceObject from "./SpaceObject";
+import Earth from "./Earth";
 
 const spaceStyle = {
   width: '100%',
   position: 'relative',
+};
+
+const earthData = {
+  display: {
+    size: 2,
+    distance: 0,
+    angle: 0,
+  }
 };
 
 export default class Space extends Component {
@@ -13,8 +22,8 @@ export default class Space extends Component {
       <Measure>
         { dimensions =>
           <div style={spaceStyle} className="space">
-            <SpaceObject dimensions={dimensions} data={{display: {size: 2, distance: 0, angle: 0}}}>
-              <img src="/assets/tierra_nubes.svg" alt="Earth" style={{maxWidth: '100%', maxHeight: '100%'}}/>
+            <SpaceObject dimensions={dimensions} data={earthData}>
+              <Earth/>
             </SpaceObject>
             {this.props.neos.map((neo, index) =>
               <SpaceObject key={index} dimensions={dimensions} data={neo} selectNeo={this.props.selectNeo}/>
