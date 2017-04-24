60271574c79fef137e15828b47d4b298c0269023
Gonza <gonzalo.medinarosa@litebyte.us>
2017-04-23 23:03:25 -0300

    Show progress bar while loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import injectTapEventPlugin from "react-tap-event-plugin";
 import React, {Component} from "react";
 import MediaQuery from "react-responsive";
+import LinearProgress from "material-ui/LinearProgress";
+import {Set} from "immutable";
 import theme from "./components/theme";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
@@ -15,6 +17,13 @@ injectTapEventPlugin();
 
 window.firstLoad = true;
 
+const progressStyle = {
+  position: 'absolute',
+  top: 46,
+  background: '#0a0a0a',
+  zIndex: 1401,
+};
+
 class App extends Component {
 
   constructor(props) {
@@ -24,6 +33,7 @@ class App extends Component {
       neos: [],
       date: new Date(),
       aboutUs: false,
+      loading: Set(),
     };
 
     this.getNeos(this.state.date);
@@ -37,9 +47,16 @@ class App extends Component {
   };
 
   getNeos(date) {
+    if (!window.firstLoad)
+      this.setState({
+        loading: this.state.loading.add(date.toISOString().substr(0, 10))
+      });
     NeoServices.getNeos(date)
       .then(neos => {
-        this.setState({neos});
+        this.setState({
+          neos: neos,
+          loading: this.state.loading.remove(date.toISOString().substr(0, 10)),
+        });
       })
       .catch(err => console.log(err));
   }
@@ -55,6 +72,7 @@ class App extends Component {
       <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
         <div style={{height: "100vh", minWidth: 300}}>
           <AboutUs open={this.state.aboutUs} setOpen={this.handleAbout}/>
+          { this.state.loading.size > 0 ? <LinearProgress style={progressStyle} color="#122b3a"/> : null}
           <Header date={this.state.date} handleDate={this.handleDate} handleAbout={this.handleAbout}/>
           <Main neos={this.state.neos} handleAbout={this.handleAbout}/>
           <MediaQuery component={Footer} minWidth={500} minHeight={400}/>
